9d0c00a25ad3c147acdde74eb7fe89f3cdaba880
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-06-16 18:34:43 -0500

    Added clear button and finalized layout

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ function add(x,y) { return x+y }
 function subtract(x,y) { return x-y }
 function multiply(x,y) { return x*y }
 function divide(x,y) { return x/y }
+function operate(x,y,o) {
+  if (o === "+") {
+    return add(x,y);
+  } else if (o === "-") {
+    return subtract(x,y);
+  } else if (o === "*") {
+    return multiply(x,y);
+  } else if (o === "/") {
+    return divide(x,y);
+  }
+}
